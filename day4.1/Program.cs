f9e0e257a778341917eeae8ae38f7bc3fd45240c
Roman Zdorovenko <75687097+ZdorovenkoR@users.noreply.github.com>
2020-12-25 20:11:37 +0600

    Update Program.cs

--- day4.1/Program.cs
+++ day4.1/Program.cs
@@ -10,6 +10,18 @@ namespace day4._1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите фамилию");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Введите имя");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Введите отчетсво");
+            string patronymic = Console.ReadLine();
+            string fullName = GetFullName(firstName, lastName, patronymic);
+            Console.WriteLine(fullName);
+        }
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return $"{firstName} {lastName} {patronymic}";
         }
     }
 }
